13577fe3359c51d52ae99d85117696321d6400c3
Arthur Araújo Rabelo <126261251+arthurarabelo@users.noreply.github.com>
2023-02-27 20:05:09 -0300

    Update array_exercise1.py

--- array_exercise1.py
+++ array_exercise1.py
@@ -1,9 +1,11 @@
 """
+
 Include in a program a method for
 read the size of an array to integers from the keyboard,
 as well as all of its elements, ensuring it only has positive and even values.
 Check that the size (or dimension) is not null or negative.
 To test, read different amounts of data.
+
 """
 
 def read_array():
